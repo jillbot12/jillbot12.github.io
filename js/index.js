7d5fb623c14791170b73507f1289e615635284f5
jillbot12 <jill.allyn.peterson@gmail.com>
2016-03-08 15:01:05 -0500

    fixed spacing with nav

--- js/index.js
+++ js/index.js
@@ -15,6 +15,30 @@ $grid.isotope({
 });
 
 
+//sticky sidebar
+
+$(function() {
+
+    var $sidebar   = $(".sidebar"), 
+        $window    = $(window),
+        offset     = $sidebar.offset(),
+        topPadding = 15;
+
+    $window.scroll(function() {
+        if ($window.scrollTop() > offset.top) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 0
+            });
+        }
+    });
+    
+});
+
+
 
 //filter for categories and types at the same time
 
